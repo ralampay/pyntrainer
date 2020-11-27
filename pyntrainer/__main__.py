81ee089b84f7eae633836e421ee188281e1ff307
Raphael Alampay <raphael.alampay@gmail.com>
2020-11-28 01:56:51 +0800

    added thresholding for prediction

--- pyntrainer/__main__.py
+++ pyntrainer/__main__.py
@@ -12,7 +12,7 @@ from lib.autoencoder import Autoencoder
 parser = argparse.ArgumentParser(description="PynTrainer: Stochastic Autoencoder trainer program")
 
 parser.add_argument("--mode", help="Mode to be used", choices=["train", "predict"], type=str, default="train")
-parser.add_argument("--input-file", help="Input csv file for training", required=True)
+parser.add_argument("--input-file", help="Input csv file for training")
 parser.add_argument("--model-file", help="Output model file", type=str, const=1, nargs='?', default="output.pth.tar")
 parser.add_argument("--chunk-size", help="Chunk size for reading large files", type=int, const=1, nargs='?', default=100)
 parser.add_argument("--layers", help='Layers for autoencoder', type=int, nargs='+', required=True)
@@ -21,6 +21,7 @@ parser.add_argument("--lr", help='Learning rate', type=float, default=0.001)
 parser.add_argument("--epochs", help='Number of epochs', type=int, default=100)
 parser.add_argument("--batch-size", help='Batch size', type=int, default=5)
 parser.add_argument("--cont", help='Continue training from model file', type=bool, default=False)
+parser.add_argument("--eval-file", help='File to evaluate. Should have the format x1,x2,x3...y', type=str)
 
 args = parser.parse_args()
 
@@ -35,12 +36,14 @@ if __name__ == '__main__':
     epochs      = args.epochs
     batch_size  = args.batch_size
     cont        = args.cont
+    eval_file   = args.eval_file
 
     print("Initializing autoencoder...")
     net = Autoencoder(layers=layers)
     print(net)
 
     if mode == "train":
+        print("Loading training data...")
         data = pd.DataFrame()
 
         for chunk in pd.read_csv(input_file, header=None, chunksize=chunk_size):
@@ -61,6 +64,33 @@ if __name__ == '__main__':
         net.save(model_file)
 
     elif mode == "predict":
+        print("Loading training data...")
+        data = pd.DataFrame()
+
+        for chunk in pd.read_csv(eval_file, header=None, chunksize=chunk_size):
+            print("Reading chunk...")
+            data = data.append(chunk)
+
+        print("Parsing evaluation file %s..." % (eval_file))
+
+        positive_data = torch.tensor(data[data[len(data.columns) - 1] == 1].iloc[:,:len(data.columns) - 1].values).float()
+        negative_data = torch.tensor(data[data[len(data.columns) - 1] == 0].iloc[:,:len(data.columns) - 1].values).float()
+
+        print("Number of positive data: %d" % (len(positive_data)))
+        print("Number of negative data: %d" % (len(negative_data)))
+
         print("Loading model...")
         net.load(model_file)
         print(net)
+
+        positive_predictions = net.predict(positive_data)
+        negative_predictions = net.predict(negative_data)
+
+        print("Positive predictions")
+        print(positive_predictions)
+
+        print("Negative predictions")
+        print(negative_predictions)
+
+        print("Positive Accuracy: %0.4f" % (len(positive_predictions[positive_predictions == 1]) / len(positive_predictions)))
+        print("Negative Accuracy: %0.4f" % (len(negative_predictions[negative_predictions == -1]) / len(negative_predictions)))
--- pyntrainer/lib/autoencoder.py
+++ pyntrainer/lib/autoencoder.py
@@ -4,10 +4,17 @@ import torch.nn.functional as F
 import torch.optim as optim
 import sys
 import os
+import random
 sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
 from torch.utils.data import Dataset, DataLoader
+from scipy.stats import iqr
+import numpy as np
 
 from abstract_dataset import AbstractDataset
+
+from utils import create_histogram
+from utils import htb
+from utils import fetch_threshold
 
 class Autoencoder(nn.Module):
     def __init__(self, layers=[]):
@@ -47,7 +54,9 @@ class Autoencoder(nn.Module):
         state = {
             'state_dict': self.state_dict(), 
             'optimizer': self.optimizer.state_dict(), 
-            'loss': self.loss
+            'loss': self.loss,
+            'mu_tensor': self.mu_tensor,
+            'optimal_threshold': self.optimal_threshold
         }
 
         torch.save(state, filename)
@@ -59,8 +68,70 @@ class Autoencoder(nn.Module):
 
         self.optimizer  = state['optimizer']
         self.loss       = state['loss']
+        self.mu_tensor  = state['mu_tensor']
+        self.optimal_threshold = state['optimal_threshold']
 
-    def train(self, x, epochs=100, lr=0.005, batch_size=5, loss="mse"):
+    def errors(self, x):
+        x_hat = self.forward(x)
+
+        if self.loss == "mse":
+            err = (x_hat - x).pow(2).sum(dim=1)
+        elif self.loss == "aml":
+            err = (x_hat - x).pow(2).sum(dim=1) + (x_hat - self.mu_tensor).pow(2).sum(dim=1)
+        else:
+            err = (x_hat - x).pow(2).sum(dim=1)
+
+        return err.detach().numpy()
+
+    def predict(self, x):
+        errors = self.errors(x)
+
+        bool_arr = errors >= self.optimal_threshold
+
+        return np.array([-1 if elem else 1 for elem in bool_arr])
+        
+
+    def synthesize(self, x, num_samples=100, n_dim=5):
+        z = self.encode(x)
+
+        z_set = z[torch.randperm(len(z))[:num_samples]]
+
+        for i in range(len(z_set)):
+            rand_indices = torch.randperm(len(z_set[i]))[:n_dim]
+
+            for r_i in rand_indices:
+                z_set[i][r_i] = random.uniform(0, 1)
+
+        return self.decode(z_set)
+
+    def set_optimal_threshold(self, x, add_syn=True, num_samples=100, n_dim=5):
+        errors = self.errors(x)
+
+        if add_syn:
+            syn_errors = self.errors(self.synthesize(x, num_samples=num_samples, n_dim=n_dim))
+
+            errors = np.concatenate((errors, syn_errors), axis=0)
+
+        # Calculate the number of bins according to Freedman-Diaconis rule
+        bin_width   = 2 * iqr(errors) / np.power(len(errors), (1/3))
+        num_bins    = (np.max(errors) - np.min(errors)) / bin_width
+
+        hist, bins = create_histogram(errors, num_bins=num_bins, step=bin_width)
+        occurences = [float(o) for o in hist.tolist()]
+
+        breaks = htb(hist)
+
+        possible_thresholds = []
+
+        for b in breaks:
+            t = fetch_threshold(bins, hist, b)
+            possible_thresholds.append(t)
+
+        self.optimal_threshold = max(possible_thresholds)
+
+        return self.optimal_threshold
+
+    def train(self, x, epochs=100, lr=0.005, batch_size=5, loss="mse", with_thresholding=True):
         self.loss = loss
 
         # Get the mean vector
@@ -92,3 +163,15 @@ class Autoencoder(nn.Module):
 
             curr_loss = curr_loss / num_iterations
             print("=> Epoch: %i\tLoss: %0.5f" % (epoch + 1, curr_loss.item()))
+
+        if with_thresholding:
+            print("Setting optimal threshold...")
+
+            if self.loss == "mse":
+                add_syn = False
+            elif self.loss == "aml":
+                add_syn = True
+
+            self.set_optimal_threshold(x, add_syn=add_syn)
+
+            print("Optimal threshold: %0.4f" % (self.optimal_threshold))
