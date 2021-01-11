53f1013c9b9ea99241027529251b4f7cfbfec776
Raphael Alampay <raphael.alampay@gmail.com>
2021-01-11 10:15:29 +0800

    added seismic tests without nn

---
+++ bin/run_prob_seismic_test.sh
@@ -0,0 +1,60 @@
+# DATASET: seismic
+# Layers: 18 15
+# Batch Size: 100
+# Percentages to test: 0.01, 0.02, 0.03, 0.04, 0.05
+
+# prob
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.01 --printout ~/results/prob/seismic-1-1.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.01 --printout ~/results/prob/seismic-1-2.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.01 --printout ~/results/prob/seismic-1-3.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.01 --printout ~/results/prob/seismic-1-4.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.01 --printout ~/results/prob/seismic-1-5.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.01 --printout ~/results/prob/seismic-1-6.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.01 --printout ~/results/prob/seismic-1-7.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.01 --printout ~/results/prob/seismic-1-8.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.01 --printout ~/results/prob/seismic-1-9.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.01 --printout ~/results/prob/seismic-1-10.csv
+
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.02 --printout ~/results/prob/seismic-2-1.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.02 --printout ~/results/prob/seismic-2-2.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.02 --printout ~/results/prob/seismic-2-3.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.02 --printout ~/results/prob/seismic-2-4.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.02 --printout ~/results/prob/seismic-2-5.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.02 --printout ~/results/prob/seismic-2-6.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.02 --printout ~/results/prob/seismic-2-7.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.02 --printout ~/results/prob/seismic-2-8.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.02 --printout ~/results/prob/seismic-2-9.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.02 --printout ~/results/prob/seismic-2-10.csv
+
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.03 --printout ~/results/prob/seismic-3-1.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.03 --printout ~/results/prob/seismic-3-2.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.03 --printout ~/results/prob/seismic-3-3.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.03 --printout ~/results/prob/seismic-3-4.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.03 --printout ~/results/prob/seismic-3-5.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.03 --printout ~/results/prob/seismic-3-6.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.03 --printout ~/results/prob/seismic-3-7.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.03 --printout ~/results/prob/seismic-3-8.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.03 --printout ~/results/prob/seismic-3-9.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.03 --printout ~/results/prob/seismic-3-10.csv
+
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.04 --printout ~/results/prob/seismic-4-1.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.04 --printout ~/results/prob/seismic-4-2.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.04 --printout ~/results/prob/seismic-4-3.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.04 --printout ~/results/prob/seismic-4-4.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.04 --printout ~/results/prob/seismic-4-5.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.04 --printout ~/results/prob/seismic-4-6.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.04 --printout ~/results/prob/seismic-4-7.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.04 --printout ~/results/prob/seismic-4-8.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.04 --printout ~/results/prob/seismic-4-9.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.04 --printout ~/results/prob/seismic-4-10.csv
+
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.05 --printout ~/results/prob/seismic-5-1.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.05 --printout ~/results/prob/seismic-5-2.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.05 --printout ~/results/prob/seismic-5-3.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.05 --printout ~/results/prob/seismic-5-4.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.05 --printout ~/results/prob/seismic-5-5.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.05 --printout ~/results/prob/seismic-5-6.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.05 --printout ~/results/prob/seismic-5-7.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.05 --printout ~/results/prob/seismic-5-8.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.05 --printout ~/results/prob/seismic-5-9.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/seismic.csv --layers 18 15 --eval-cat prob --batch-size 100 --neg-cont 0.05 --printout ~/results/prob/seismic-5-10.csv
