c70adb75737fdb3fd1935a08e0e02c2d4d0d5d1e
Raphael Alampay <raphael.alampay@gmail.com>
2021-01-11 08:48:07 +0800

    added mammography tests without nn

---
+++ bin/run_ensemble_mammography_test.sh
@@ -0,0 +1,60 @@
+# DATASET: mammography
+# Layers: 6 4
+# Batch Size: 10
+# Percentages to test: 0.01, 0.02, 0.03, 0.04, 0.05
+
+# ensemble
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.01 --printout ~/results/ensemble/mammography-1-1.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.01 --printout ~/results/ensemble/mammography-1-2.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.01 --printout ~/results/ensemble/mammography-1-3.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.01 --printout ~/results/ensemble/mammography-1-4.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.01 --printout ~/results/ensemble/mammography-1-5.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.01 --printout ~/results/ensemble/mammography-1-6.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.01 --printout ~/results/ensemble/mammography-1-7.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.01 --printout ~/results/ensemble/mammography-1-8.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.01 --printout ~/results/ensemble/mammography-1-9.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.01 --printout ~/results/ensemble/mammography-1-10.csv
+
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.02 --printout ~/results/ensemble/mammography-2-1.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.02 --printout ~/results/ensemble/mammography-2-2.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.02 --printout ~/results/ensemble/mammography-2-3.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.02 --printout ~/results/ensemble/mammography-2-4.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.02 --printout ~/results/ensemble/mammography-2-5.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.02 --printout ~/results/ensemble/mammography-2-6.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.02 --printout ~/results/ensemble/mammography-2-7.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.02 --printout ~/results/ensemble/mammography-2-8.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.02 --printout ~/results/ensemble/mammography-2-9.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.02 --printout ~/results/ensemble/mammography-2-10.csv
+
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.03 --printout ~/results/ensemble/mammography-3-1.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.03 --printout ~/results/ensemble/mammography-3-2.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.03 --printout ~/results/ensemble/mammography-3-3.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.03 --printout ~/results/ensemble/mammography-3-4.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.03 --printout ~/results/ensemble/mammography-3-5.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.03 --printout ~/results/ensemble/mammography-3-6.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.03 --printout ~/results/ensemble/mammography-3-7.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.03 --printout ~/results/ensemble/mammography-3-8.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.03 --printout ~/results/ensemble/mammography-3-9.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.03 --printout ~/results/ensemble/mammography-3-10.csv
+
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.04 --printout ~/results/ensemble/mammography-4-1.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.04 --printout ~/results/ensemble/mammography-4-2.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.04 --printout ~/results/ensemble/mammography-4-3.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.04 --printout ~/results/ensemble/mammography-4-4.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.04 --printout ~/results/ensemble/mammography-4-5.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.04 --printout ~/results/ensemble/mammography-4-6.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.04 --printout ~/results/ensemble/mammography-4-7.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.04 --printout ~/results/ensemble/mammography-4-8.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.04 --printout ~/results/ensemble/mammography-4-9.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.04 --printout ~/results/ensemble/mammography-4-10.csv
+
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.05 --printout ~/results/ensemble/mammography-5-1.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.05 --printout ~/results/ensemble/mammography-5-2.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.05 --printout ~/results/ensemble/mammography-5-3.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.05 --printout ~/results/ensemble/mammography-5-4.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.05 --printout ~/results/ensemble/mammography-5-5.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.05 --printout ~/results/ensemble/mammography-5-6.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.05 --printout ~/results/ensemble/mammography-5-7.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.05 --printout ~/results/ensemble/mammography-5-8.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.05 --printout ~/results/ensemble/mammography-5-9.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/mammography.csv --layers 6 4 --eval-cat ensemble --batch-size 10 --neg-cont 0.05 --printout ~/results/ensemble/mammography-5-10.csv
