bee88fabab83d63c1e61e90668c4170f83ad08fc
Raphael Alampay <raphael.alampay@gmail.com>
2021-01-11 10:35:33 +0800

    added shuttle tests without nn

---
+++ bin/run_ensemble_shuttle_test.sh
@@ -0,0 +1,60 @@
+# DATASET: shuttle
+# Layers: 9 6
+# Batch Size: 100
+# Percentages to test: 0.01, 0.02, 0.03, 0.04, 0.05
+
+# ensemble
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.01 --printout ~/results/ensemble/shuttle-1-1.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.01 --printout ~/results/ensemble/shuttle-1-2.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.01 --printout ~/results/ensemble/shuttle-1-3.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.01 --printout ~/results/ensemble/shuttle-1-4.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.01 --printout ~/results/ensemble/shuttle-1-5.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.01 --printout ~/results/ensemble/shuttle-1-6.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.01 --printout ~/results/ensemble/shuttle-1-7.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.01 --printout ~/results/ensemble/shuttle-1-8.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.01 --printout ~/results/ensemble/shuttle-1-9.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.01 --printout ~/results/ensemble/shuttle-1-10.csv
+
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.02 --printout ~/results/ensemble/shuttle-2-1.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.02 --printout ~/results/ensemble/shuttle-2-2.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.02 --printout ~/results/ensemble/shuttle-2-3.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.02 --printout ~/results/ensemble/shuttle-2-4.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.02 --printout ~/results/ensemble/shuttle-2-5.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.02 --printout ~/results/ensemble/shuttle-2-6.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.02 --printout ~/results/ensemble/shuttle-2-7.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.02 --printout ~/results/ensemble/shuttle-2-8.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.02 --printout ~/results/ensemble/shuttle-2-9.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.02 --printout ~/results/ensemble/shuttle-2-10.csv
+
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.03 --printout ~/results/ensemble/shuttle-3-1.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.03 --printout ~/results/ensemble/shuttle-3-2.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.03 --printout ~/results/ensemble/shuttle-3-3.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.03 --printout ~/results/ensemble/shuttle-3-4.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.03 --printout ~/results/ensemble/shuttle-3-5.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.03 --printout ~/results/ensemble/shuttle-3-6.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.03 --printout ~/results/ensemble/shuttle-3-7.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.03 --printout ~/results/ensemble/shuttle-3-8.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.03 --printout ~/results/ensemble/shuttle-3-9.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.03 --printout ~/results/ensemble/shuttle-3-10.csv
+
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.04 --printout ~/results/ensemble/shuttle-4-1.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.04 --printout ~/results/ensemble/shuttle-4-2.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.04 --printout ~/results/ensemble/shuttle-4-3.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.04 --printout ~/results/ensemble/shuttle-4-4.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.04 --printout ~/results/ensemble/shuttle-4-5.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.04 --printout ~/results/ensemble/shuttle-4-6.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.04 --printout ~/results/ensemble/shuttle-4-7.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.04 --printout ~/results/ensemble/shuttle-4-8.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.04 --printout ~/results/ensemble/shuttle-4-9.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.04 --printout ~/results/ensemble/shuttle-4-10.csv
+
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.05 --printout ~/results/ensemble/shuttle-5-1.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.05 --printout ~/results/ensemble/shuttle-5-2.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.05 --printout ~/results/ensemble/shuttle-5-3.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.05 --printout ~/results/ensemble/shuttle-5-4.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.05 --printout ~/results/ensemble/shuttle-5-5.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.05 --printout ~/results/ensemble/shuttle-5-6.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.05 --printout ~/results/ensemble/shuttle-5-7.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.05 --printout ~/results/ensemble/shuttle-5-8.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.05 --printout ~/results/ensemble/shuttle-5-9.csv && \
+python -m pyntrainer --mode eval --input-file https://happy-research.s3-ap-southeast-1.amazonaws.com/shuttle.csv --layers 9 6 --eval-cat ensemble --batch-size 100 --neg-cont 0.05 --printout ~/results/ensemble/shuttle-5-10.csv
